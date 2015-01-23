b7ebca9f8d06070585010799d5aeb123ae341703
mieva <michelaievamio@gmail.com>
2015-01-22 16:19:36 -0800

    R script: list dir and mean

---
+++ pollutantmean.R
@@ -0,0 +1,42 @@
+setwd("C:/Users/Michela/Desktop/ieva/DATASCIENCE/datasciencecoursera")
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate the
+  ## mean; either "sulfate" or "nitrate".
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return the mean of the pollutant across all monitors list
+  ## in the 'id' vector (ignoring NA values)
+  
+  # set working directory
+  if(grep("specdata", directory) == 1) {
+    directory <- ("./specdata/")
+  }
+  
+  mean_vector <- c()
+  
+  # find all files in the specdata folder
+  all_files <- as.character( list.files(directory) )
+  file_paths <- paste(directory, all_files, sep="")
+    
+  for(i in id) {
+      current_file <- read.csv(file_paths[i], header=T, sep=",")
+      
+      #pollutant
+      
+      na_removed <- current_file[!is.na(current_file[, pollutant]), pollutant]
+      mean_vector <- c(mean_vector, na_removed)
+    
+  }
+  
+  result <- mean(mean_vector)
+  return(round(result, 3)) 
+  
+}
+

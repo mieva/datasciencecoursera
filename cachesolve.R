0034cb32e0ac5a05f7d0c232945c5ee31b6c296d
mieva <michelaievamio@gmail.com>
2015-01-22 16:22:43 -0800

    R script: lexical scoping inverse matrix solve

---
+++ cachesolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinv(inv)
+        inv
+}
